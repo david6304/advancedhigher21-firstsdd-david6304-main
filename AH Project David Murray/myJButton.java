ec86ae9a79001aa7c8482341c89348b8a71a5eca
David Murray <David6304murray@gmail.com>
2022-02-25 09:29:33 +0000

    finished commenting, need to add button to display

--- AH Project David Murray/myJButton.java
+++ AH Project David Murray/myJButton.java
@@ -5,14 +5,17 @@ import java.awt.event.ActionEvent;
 public class myJButton extends JButton implements ActionListener{
     private boolean pressed = false;
 
+    //sets pressed to true when button is pressed
     public void actionPerformed(ActionEvent e) {
         this.pressed = true;
     }
 
+    //sets pressed to false 
     public void setPressedFalse() {
         this.pressed = false;
     }
 
+    //returns the value of pressed 
     public boolean isPressed() {
         return this.pressed;
     }
